19eac697b8fcc16eb7ed041afbcc2214fc475b69
Zhurkin-Dmitriy <zhurkindo@gmail.com>
2021-05-19 11:13:41 +0300

    practice form tests

---
+++ src/test/java/ToolsQAPracticeForm.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class ToolsQAPracticeForm {
+
+    @Test
+    void selenideSearchTest() {
+        open("https://demoqa.com/automation-practice-form");
+
+    }
+}
